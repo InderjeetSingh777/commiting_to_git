c4d5ef8e63067276333783b6adb08d368d8c17af
Inderjeet <billinginderjeetsigh#gmail.com>
2018-08-28 19:10:54 +0530

    commiting to gihub remote

--- gitHubDemo/src/gitHubDemo/Demo.java
+++ gitHubDemo/src/gitHubDemo/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("demo github.com"); 
-		System.out.print("hello world");
+//		System.out.print("hello world");
 
 	}
 
